75e6e0520832fa1b92a60e6e8be829d3165904e1
hasibulislam999 <hasib143sl@gmail.com>
2022-08-15 01:26:09 +0600

    index modified

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+
 /**
  * Title: Server for Luxury Living app
  * Description: A comprehensive back-end to maintain Luxury Living app
@@ -7,40 +9,63 @@
 
 // required middleware
 const express = require("express");
-var cors = require("cors")
+var cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 // connect middleware
 app.use(cors());
 app.use(express.json());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
+// internal imports
+const customer = require("./routes/customer");
+const admin = require("./routes/admin");
 
 // mongodb connectivity
 const uri = process.env.DB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
     try {
         await client.connect();
         console.log("MongoDB connected");
-        const collection = client.db("test").collection("devices");
+
+        // driver database
+        const userCollection = client.db("people").collection("user");
+
+        app.post("/user/", async (req, res) => {
+            res.status(201).send(await userCollection.insertOne(req.body));
+        });
+
+        app.get("/user/:name", async (req, res) => {
+            res.status(200).send(await userCollection.find({name: req.params.name}).toArray());
+        });
     } catch {
         // await client.close();
     }
-} run().catch(console.dir);
+}
+run().catch(console.dir);
 
 // enable requests
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     // res.status(200).json({ message: "Luxury Living app connected" });
     res.status(201).render("index", {
         name: process.env.APP_NAME,
     });
-})
+});
+
+// routing setup
+app.use("/admin", admin);
+app.use("/customer", customer);
 
 app.listen(port, () => {
     console.log(`Luxury Living app listening on port ${port}`);
-})
+});
