75cbb0f62caca06209a3e1282e3f71e24e06048e
hasibulislam999 <hasib143sl@gmail.com>
2022-08-17 08:10:34 +0600

    driver code updated with respect mondo uri

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const customer = require("./routes/customer");
 const admin = require("./routes/admin");
 
 // mongodb connectivity
-const uri = process.env.DB_URI;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@luxuryliving.38sh1rq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
